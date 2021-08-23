4b313fd1caa96d2bf3b24a9f270c6a34b955c7ce
Wing Ng <tockets.alt@gmail.com>
2021-08-23 11:10:24 +0100

    Moved the (Quasar) data table from the view 'Testing' to a separate component 'DataTable'. Added some styling, and the ability for selecting multiple rows. Working on alternating row colours.

---
+++ src/components/DataTable.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      title="Patients"
+      :rows="rows"
+      :columns="columns"
+      :separator="separator"
+      :selected-rows-label="getSelectedString"
+      selection="multiple"
+      v-model:selected="selected"
+      row-key="patientID"
+      dark
+      table-header-class="bg-indigo-9"
+      table-class="bg-indigo-14"
+      card-class="bg-indigo-10"
+    >
+      <!-- <q-tr :class="(props.rowIndex%2)===0?'bg-grey':'bg-white'">
+        <div class="bg-white"></div>
+        <div class="bg-grey"></div>
+    </q-tr> -->
+    </q-table>
+    <!-- Debugging purposes (shows which are selected)
+    <div class="q-mt-md">
+      Selected: {{ JSON.stringify(selected) }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+const columns = [
+  {
+    name: "patientID",
+    required: true,
+    label: "Patient ID",
+    align: "left",
+    field: (row) => row.patientID,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "patientName",
+    align: "center",
+    label: "Patient Name",
+    field: "patientName",
+    sortable: true,
+  },
+  {
+    name: "studyDate",
+    label: "Study Date",
+    field: "studyDate",
+    sortable: true,
+  },
+  { name: "studyDesc", label: "Study Description", field: "studyDesc" },
+  {
+    name: "modalities",
+    label: "Modalities",
+    field: "modalities",
+    sortable: true,
+  },
+  {
+    name: "imageCount",
+    label: "No. of Images",
+    field: "imageCount",
+    sortable: true,
+  },
+];
+
+const rows = [
+  {
+    patientID: "PatientID Test",
+    patientName: "Patient Name",
+    studyDate: "20/08/21",
+    studyDesc: "CT scan of head",
+    modalities: "CT",
+    imageCount: "54",
+  },
+  {
+    patientID: "John",
+    patientName: "Doe",
+    studyDate: "21/08/21",
+    studyDesc: "CT scan of chest",
+    modalities: "CT",
+    imageCount: "40",
+  },
+];
+
+export default {
+  name: "DataTable",
+  setup() {
+      const selected = ref([])
+
+      return {
+        selected,
+        separator: ref("cell"),
+        columns,
+        rows,
+
+        getSelectedString () {
+            return selected.value.length === 0 ? '' : `${selected.value.length} record${selected.value.length > 1 ? 's' : ''} selected of ${rows.length}`
+        }
+    };
+  },
+};
+</script>
+
+<style>
+/* .q-table tbody td:after {
+  background: rgba(255, 0, 0, 0.2);
+}
+.q-table tbody td:before {
+  background: rgba(255, 0, 0, 0.1);
+} */
+.bg-white {
+  background: white;
+}
+
+.bg-grey {
+  background: tomato;
+}
+</style>
--- src/views/Testing.vue
+++ src/views/Testing.vue
@@ -1,53 +1,25 @@
 <template>
-  <div class="q-pa-md">
-    <q-table title="Patients" :rows="rows" :columns="columns" row-key="name" />
-  </div>
+  <DataTable />
   <router-link to="/">Home</router-link>
 </template>
 
 <script>
-const columns = [
-  {
-    name: 'patientID',
-    required: true,
-    label: 'Patient ID',
-    align: 'left',
-    field: row => row.patientID,
-    format: val => `${val}`,
-    sortable: true
-  },
-  { name: 'patientName', align: 'center', label: 'Patient Name', field: 'patientName', sortable: true},
-  { name: 'studyDate', label: 'Study Date', field: 'studyDate', sortable: true},
-  { name: 'studyDesc', label: 'Study Description', field: 'studyDesc'},
-  { name: 'modalities', label: 'Modalities', field: 'modalities', sortable: true },
-  { name: 'imageCount', label: 'No. of Images', field: 'imageCount', sortable: true }
-]
-
-const rows = [
-  {
-    patientID: 'PatientID Test',
-    patientName: 'Patient Name',
-    studyDate: '20/08/21',
-    studyDesc: 'CT scan of head',
-    modalities: 'CT',
-    imageCount: '54'
-  },
-  {
-    patientID: 'John',
-    patientName: 'Doe',
-    studyDate: '21/08/21',
-    studyDesc: 'CT scan of chest',
-    modalities: 'CT',
-    imageCount: '40'
-  },
-]
+import DataTable from '../components/DataTable.vue'
 
 export default {
-  setup () {
-    return {
-      columns,
-      rows
-    }
+  components: {
+    DataTable
   }
 }
 </script>
+
+<style>
+.container {
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+</style>
